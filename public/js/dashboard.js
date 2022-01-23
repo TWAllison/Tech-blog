511b5a9f19d5f097bc0751c1a37325aaa16d977e
TWAllison <tylerallison55@gmail.com>
2022-01-23 03:26:08 -0700

    add file structure to /public, add handlers to dashboard.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,36 @@
+const addButtonHandler = async (event) => {
+  document.location.replace("dashboard/newArticle");
+};
+
+const updateButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    document.location.replace(`/dashboard/updateArticle/${id}`);
+  } else {
+    alert("data-id not found");
+  }
+};
+
+const deleteButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/articles/${id}`, { method: "DELETE" });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("failed to delete article!");
+    }
+  } else {
+    alert("article data-id not found");
+  }
+};
+
+document.querySelector("#btn-add").addEventListener("click", addButtonHandler);
+document
+  .querySelector("btn-update")
+  .foreach((btn) => btn.addEventListener("click", updateButtonHandler));
+document
+  .querySelector("btn-delete")
+  .foreach((btn) => btn.addEventListener("click", deleteButtonHandler));
