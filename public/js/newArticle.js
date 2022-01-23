d5a8efb940e9436c151029fbd5c4e485360a9ffe
TWAllison <tylerallison55@gmail.com>
2022-01-23 03:59:35 -0700

    public/ js / new article handlers

---
+++ public/js/newArticle.js
@@ -0,0 +1,32 @@
+const e = require("express");
+
+const newArticleHandler = async (event) => {
+    event.preventDefault();
+
+const articleTitle = document.getElementById('newArticleTitle').value.trim();
+const articleBody = document.getElementById('newArticleContent').value.trim();
+
+ if (articleTitle && articleBody) {
+     const response = await fetch('/api/articles/', {
+         method: 'POST',
+         body: JSON.stringify({ articleTitle, articleBody }),
+         headers: { "content-Type": "application/json" },
+     });
+     if (response.ok) {
+         document.location.replace('/dashboard');
+     } else { alert(response.status.text)}
+ }
+};
+
+const cancelBtnHandler = async (event) => {
+    event.preventDefault();
+    document.location.replace('/dashboard');
+}
+
+document
+.querySelector('.newArticleForm')
+.addEventListener('submit', newArticleHandler);
+
+document
+.querySelector('.newArticleForm')
+.addEventListener('reset', cancelBtnHandler);
