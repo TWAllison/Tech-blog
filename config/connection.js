b90f8540d71487c6b61e4db61e114212c631c20c
TWAllison <tylerallison55@gmail.com>
2022-01-25 04:19:29 -0700

    trying to establish connction

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 /*let sequelize;
 if(process.env.JAWSDB_URL) {
@@ -18,11 +18,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3006,
     });
 
-  
- 
 module.exports = sequelize;
