a14b5c2f9e6a2c41a5f1816717da9b6ceb2cdcf7
TWAllison <tylerallison55@gmail.com>
2022-01-26 04:52:56 -0700

    testing changes in server.js

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 // import controllers, utils , path, etc.
 const path = require('path');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
 const express = require('express');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const app = express();
-const PORT = process.env.PORT || 3005;
 
 //set up handlebars engine with helpers 
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'i like tacos',
+    secret: 'Super secret secret',
     cookies: {},
     resave: false,
     saveUninitialized: true,
@@ -27,6 +25,8 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
 //set up template engine for express.js
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -35,6 +35,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+
+const routes = require('./controllers/');
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
